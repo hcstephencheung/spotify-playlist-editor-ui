861797752503c73270783d82538e156c307bd3b3
hocheung <hocheung@bestbuycanada.ca>
2019-02-04 17:23:19 -0800

    Add redirect routes using reach router

--- src/js/Token.js
+++ src/js/Token.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as queryString from "query-string";
 import * as Cookies from "js-cookie";
+import { Redirect } from "@reach/router";
 
 const ENDPOINT = "http://localhost:8888/appCallback";
 const FETCH_OPTIONS = {
@@ -13,28 +14,39 @@ class Token extends React.Component {
 
         this.state = {
             code: '',
-            state: ''
+            state: '',
+            redirectReady: false
         }
     }
 
-    async redirectWithCode (code, state) {
+    async redirectWithAccess () {
+        const queryParams = queryString.parse(this.props.location.search);
+        const { code, state } = queryParams;
+
         let data = await fetch(ENDPOINT + `?code=${code}&state=${state}`, FETCH_OPTIONS);
         const { ac, acKey } = data;
         Cookies.set(acKey, ac);
 
-        location = '/playlists';
+        this.setState({
+            redirectReady: true
+        });
+    }
+
+    async componentDidMount() {
+        this.redirectWithAccess();
     }
 
     render() {
-        const queryParams = queryString.parse(this.props.location.search);
-        const { code, state } = queryParams;
-        this.redirectWithCode(code, state);
-
-        return (
-            <div className="token-page">
-                <h1>Redirecting...</h1>
-            </div>
-        )
+        if (this.state.redirectReady) {
+            return <Redirect to="/playlists" />
+        } else {
+            return (
+                <div className="token-page">
+                    <h1>Redirecting...</h1>
+                </div>
+            )
+        }
+        
     }
 }
 
--- src/js/app.js
+++ src/js/app.js
@@ -3,19 +3,21 @@ import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import Playlists from "./Playlists";
 import PlaylistProfile from "./PlaylistProfile";
+import Home from "./Login";
 import Login from "./Login";
 import Token from "./Token";
 
 class App extends React.Component {
-
   render() {
     return (
       <div>
         <header>
           <Link to={`/playlists`}>Spotify Playlist Editor</Link>
+          <Link to={`/login`}>Login</Link>
         </header>
 
         <Router>
+          <Home path="/" />
           <Login path="/login" />
           <Token path="/token" />
           <Playlists path="/playlists" />
